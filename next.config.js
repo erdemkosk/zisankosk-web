9b56d783d535ce388d3c6207efcf9e844eb44914
Erdem Köşk <erdemkosk@gmail.com>
2025-04-01 04:28:39 +0300

    Adding cname

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/zisankosk',
-  assetPrefix: '/zisankosk/',
   images: {
     unoptimized: true,
   },
